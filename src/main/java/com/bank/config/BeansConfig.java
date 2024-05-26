acfdff97f786df1470ef7e19055fbcd5ae029b51
sami <samiayachi653@gmail.com>
2024-05-26 23:14:02 +0100

    fix link  front

--- src/main/java/com/bank/config/BeansConfig.java
+++ src/main/java/com/bank/config/BeansConfig.java
@@ -53,7 +53,7 @@ public class BeansConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.ORIGIN,
                 HttpHeaders.CONTENT_TYPE,
